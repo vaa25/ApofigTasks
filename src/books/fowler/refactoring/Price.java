6a9d8b3ea1b353ce1f3b39fe76507fcff04bcfbd
Alexander Vlasov <vaa25@yandex.ru>
2014-12-23 22:50:34 +0200

    getCharge moved to Price

--- src/books/fowler/refactoring/Price.java
+++ src/books/fowler/refactoring/Price.java
@@ -5,11 +5,21 @@ package books.fowler.refactoring;
  */
 abstract class Price {
     abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrensPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDRENS;
+    }
+
+    double getCharge(int daysRented) {
+        if (daysRented > 3) {
+            return 1.5 + (daysRented - 3) * 1.5;
+        } else {
+            return 1.5;
+        }
     }
 }
 
@@ -17,10 +27,22 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    double getCharge(int daysRented) {
+        if (daysRented > 2)
+            return 2 + (daysRented - 2) * 1.5;
+        else {
+            return 2;
+        }
     }
 }
