c784139e0f3ea731b2317438f8553cffd4f9a410
Alexander Vlasov <vaa25@yandex.ru>
2014-12-23 20:09:02 +0200

    amountFor added

---
+++ src/books/fowler/refactoring/CustomerLauncher.java
@@ -0,0 +1,35 @@
+package books.fowler.refactoring;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Alexander Vlasov
+ */
+public class CustomerLauncher {
+    private static List<Movie> movies;
+
+    public static void main(String[] args) {
+        setMovies();
+        Customer customer = new Customer("Alexander");
+        for (int i = 0; i < movies.size(); i++) {
+            customer.addRental(new Rental(movies.get(i), new Random().nextInt(10)));
+
+        }
+
+        System.out.println(customer.statement());
+    }
+
+    private static void setMovies() {
+        movies = new ArrayList<>();
+        movies.add(new Movie("Lost", 0));
+        movies.add(new Movie("Elementary", 1));
+        movies.add(new Movie("Forever", 1));
+        movies.add(new Movie("The hundred", 1));
+        movies.add(new Movie("Once upon a time", 2));
+        movies.add(new Movie("Ascension", 1));
+        movies.add(new Movie("Grimm", 2));
+        movies.add(new Movie("Battleship Galaxy", 0));
+    }
+}
