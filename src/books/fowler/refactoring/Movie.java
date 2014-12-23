838fa9e62054e5fbcc85b6e25d880ca0aaa81290
Alexander Vlasov <vaa25@yandex.ru>
2014-12-23 22:40:09 +0200

    Price added

--- src/books/fowler/refactoring/Movie.java
+++ src/books/fowler/refactoring/Movie.java
@@ -8,23 +8,31 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     public static final int CHILDRENS = 2;
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
         setPriceCode(priceCode);
     }
 
-    public int setPriceCode() {
-        return _priceCode;
-    }
-
     public int getPriceCode() {
-        return setPriceCode();
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String getTitle() {
---
+++ src/books/fowler/refactoring/Price.java
@@ -0,0 +1,26 @@
+package books.fowler.refactoring;
+
+/**
+ * @author Alexander Vlasov
+ */
+abstract class Price {
+    abstract int getPriceCode();
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
